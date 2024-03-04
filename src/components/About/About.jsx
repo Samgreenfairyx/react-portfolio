2baf19aefc0ae1516af09ab9bd57162391ff910b
Samgreenfairyx <150940744+Samgreenfairyx@users.noreply.github.com>
2024-03-04 13:06:06 +0000

    Just adding it all I  know it's not perfect

---
+++ src/components/About/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm learning to be a front end developer to create responsive and optimised sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Third Party API's</h3>
+              <p>
+                I have some experience utilising and connecting to third party API's
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+                I aspire to be able to design beautiful webpage designs!
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
